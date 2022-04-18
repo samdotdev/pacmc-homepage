fec031e3e10559efa42a1249993e4a3a7b697b3d
BuildTools <maquzo.mail@gmail.com>
2022-04-18 16:36:38 +0200

    added features (still need to be filled in)

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 import Logo from '../assets/kotlin.png'
+import GUI from '../assets/gui.png'
 
 const Features = () => {
   const style = {
-    wrapper: `mt-[10rem] items-center justify-center`,
+    wrapper: `mt-[5rem] items-center justify-center`,
     textContainer: `text-center mt-[7rem]`,
     title: `text-[7rem] font-extrabold text-white font-Inter md:text-[3rem]`,
     subtitle: `text-[1.3rem] font-bold text-gray-500 font-Inter`,
+    card: `flex flex-col items-center justify-center mt-[5rem]`,
+    cardContent: `flex flex-wrap items-center justify-center`,
+    cardText: `flex flex-col pl-[5rem] pr-[5rem]`,
+    cardTitle: `text-white text-[2rem] font-bold font-Inter`,
+    cardSubTitle: `text-gray-500 text-[1em] font-bold font-Inter`,
+    cardMedia: `rounded-xl shadow-2xl`,
   }
 
   return (
@@ -19,7 +26,50 @@ const Features = () => {
           manager.
         </div>
       </div>
-      <div className={style.wrapper}></div>
+      <div className={style.wrapper}>
+        <div className={style.card}>
+          <div className={style.cardContent}>
+            <div className={style.cardText}>
+              <div className={style.cardTitle}>GUI</div>
+              <div className={style.cardSubTitle}>
+                Pacmc offers a powerfull GUI to <br></br> uninstall, install &
+                manage <br></br> your Fabric mods, <br></br>all in one Project.
+              </div>
+            </div>
+            <div className={style.cardMedia}>
+              <Image src={GUI} width={500} height={400} />
+            </div>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardContent}>
+            <div className={style.cardMedia}>
+              <Image src={GUI} width={500} height={400} />
+            </div>
+            <div className={style.cardText}>
+              <div className={style.cardTitle}>GUI</div>
+              <div className={style.cardSubTitle}>
+                Pacmc offers a powerfull GUI to <br></br> uninstall, install &
+                manage <br></br> your Fabric mods, <br></br>all in one Project.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.card}>
+          <div className={style.cardContent}>
+            <div className={style.cardText}>
+              <div className={style.cardTitle}>GUI</div>
+              <div className={style.cardSubTitle}>
+                Pacmc offers a powerfull GUI to <br></br> uninstall, install &
+                manage <br></br> your Fabric mods, <br></br>all in one Project.
+              </div>
+            </div>
+            <div className={style.cardMedia}>
+              <Image src={GUI} width={500} height={400} />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
